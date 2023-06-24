e97d775c2385eae60b04154a2d3d65c140f02764
HovhannesK <hovhanneskeshishyan@gmail.com>
2023-06-24 23:59:07 +0400

    change image folder

--- src/components/the-header/TheHeader.tsx
+++ src/components/the-header/TheHeader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import AuthorImagePNG from "@/assets/author-300x300.png";
-import AuthorImageWebP from "@/assets/author-300x300.webp";
-import AuthorImageWebPSmall from "@/assets/author-150x150.webp";
+import AuthorImagePNG from "@/assets/author/png/author-300x300.png";
+import AuthorImageWebP300 from "@/assets/author/webp/author-300x300.webp";
+import AuthorImageWebP225 from "@/assets/author/webp/author-225x225.webp";
 import styles from "./TheHeader.module.scss";
 
 export const TheHeader: React.FC = () => {
@@ -11,12 +11,12 @@ export const TheHeader: React.FC = () => {
         <picture>
           <source
             media="(max-width: 720px)"
-            srcSet={AuthorImageWebPSmall}
+            srcSet={AuthorImageWebP225}
             type="image/webp"
           />
           <source
             media="(min-width: 720px)"
-            srcSet={AuthorImageWebP}
+            srcSet={AuthorImageWebP300}
             type="image/webp"
           />
           <source srcSet={AuthorImagePNG} type="image/png" />
