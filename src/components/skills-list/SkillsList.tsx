e18f1bdb7288190722b1b9bd9eae7f58018bf20c
HovhannesK <hovhanneskeshishyan@gmail.com>
2024-01-13 18:21:29 +0400

    added new skills

--- src/components/skills-list/SkillsList.tsx
+++ src/components/skills-list/SkillsList.tsx
@@ -4,6 +4,9 @@ import styles from "./SkillsList.module.scss";
 import JavaScriptIcon from "@/assets/icons/skills/javascript.png";
 import VueIcon from "@/assets/icons/skills/vue.png";
 import ReactIcon from "@/assets/icons/skills/react.png";
+import ReduxIcon from "@/assets/icons/skills/redux.png";
+import NodejsIcon from "@/assets/icons/skills/nodejs.png";
+import MongoDBIcon from "@/assets/icons/skills/mongodb.png";
 import TypeScriptIcon from "@/assets/icons/skills/typescript.png";
 import HTMLIcon from "@/assets/icons/skills/html.png";
 import CSSIcon from "@/assets/icons/skills/css.png";
@@ -13,12 +16,16 @@ import GitIcon from "@/assets/icons/skills/git.png";
 import PhotoshopIcon from "@/assets/icons/skills/photoshop.png";
 import FigmaIcon from "@/assets/icons/skills/figma.png";
 import ArticulateStorylineIcon from "@/assets/icons/skills/articulate_storyline.png";
+
 import { useSplitArrayToTwoParts } from "../../hooks/useSplitArraytoTwoParts";
 
 const SKILLS = [
   { name: "JavaScript", level: "", icon: JavaScriptIcon },
   { name: "Vue", level: "", icon: VueIcon },
   { name: "React", level: "", icon: ReactIcon },
+  { name: "Redux", level: "", icon: ReduxIcon },
+  { name: "Node.js", level: "", icon: NodejsIcon },
+  { name: "MongoDB", level: "", icon: MongoDBIcon },
   { name: "TypeScript", level: "", icon: TypeScriptIcon },
   { name: "HTML", level: "", icon: HTMLIcon },
   { name: "CSS", level: "", icon: CSSIcon },
