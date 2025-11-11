61d007abf5a464187f0e0577c76ac764bcd9ce5f
HovhannesK <hovhanneskeshishyan@gmail.com>
2025-11-11 23:20:44 +0400

    move SwitchButton inside <main> tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client"
-import {useTheme} from "next-themes"
 
-import {SwitchButton} from "@/utils/switch-button/SwitchButton";
 import {Header} from "@/components/header/Header";
 import {MainContainer} from "@/components/main-container/MainContainer";
 import {Footer} from "@/components/footer/Footer";
@@ -10,17 +8,9 @@ import {ColorTheme} from "@/utils/color-theme/ColorTheme";
 import styles from "./page.module.css";
 
 export default function App() {
-    const {theme, setTheme} = useTheme();
-
-    const toggleTheme = () => {
-        const newValue = theme === "dark" ? "light" : "dark";
-        setTheme(newValue);
-    }
-
     return (
         <div className={styles.App}>
             <ColorTheme />
-            <SwitchButton theme={theme} toggleTheme={toggleTheme}/>
             <Header/>
             <MainContainer/>
             <Footer/>
--- src/components/main-container/MainContainer.tsx
+++ src/components/main-container/MainContainer.tsx
@@ -1,18 +1,32 @@
 "use client"
+import type {FC} from "react";
 
-import React from "react";
-import styles from "./MainContainer.module.scss";
-import {ContactsList} from "../contacts-list/ContactsList";
-import {SeparatorLine} from "@/utils/separator-line/SeparatorLine";
+import {useTheme} from "next-themes";
+
+import {ContactsList} from "@/components/contacts-list/ContactsList";
 import {EducationInfo} from "@/components/education-info/EducationInfo";
-import {TwoColumns} from "@/utils/the-columns/TwoColumns";
 import {ExperienceList} from "@/components/experience-list/ExperienceList";
 import {LanguagesList} from "@/components/languages-list/LanguagesList";
 import {SkillsList} from "@/components/skills-list/SkillsList";
 
-export const MainContainer: React.FC = () => {
+import {SwitchButton} from "@/utils/switch-button/SwitchButton";
+import {SeparatorLine} from "@/utils/separator-line/SeparatorLine";
+import {TwoColumns} from "@/utils/the-columns/TwoColumns";
+
+import styles from "./MainContainer.module.scss";
+
+export const MainContainer: FC = () => {
+    const {theme, setTheme} = useTheme();
+
+    const toggleTheme = () => {
+        const newValue = theme === "dark" ? "light" : "dark";
+        setTheme(newValue);
+    }
+
     return (
         <main className={styles.main}>
+            <SwitchButton theme={theme} toggleTheme={toggleTheme}/>
+
             <SeparatorLine/>
 
             <TwoColumns>
