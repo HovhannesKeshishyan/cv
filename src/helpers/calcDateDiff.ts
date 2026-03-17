5a4be343f3ed26f9ba7d2121d17103dd654a3fb1
HovhannesK <hovhanneskeshishyan@gmail.com>
2026-03-17 15:51:20 +0400

    fix, experience duration was generated in build time and don't update

--- src/helpers/calcDateDiff.ts
+++ src/helpers/calcDateDiff.ts
@@ -1,33 +1,28 @@
 import {DateTime} from "luxon";
 
+const pluralize = (str: string, value: number): string => {
+    return value > 1 ? `${str}s` : str;
+};
+
 export const calcDateDiff = (from: string, to: string) => {
     const fromDate = DateTime.fromISO(from);
     const toDate =
-        to.toLowerCase() === "present" ? getToday() : DateTime.fromISO(to);
-    const diff = toDate.diff(fromDate, ["years", "months"]);
-    let years = diff.years;
-    let months = Math.round(diff.months);
-
-    //when months rounded to top and get 12
-    if (months === 12) {
-        years++;
-        months = 0;
-    }
-
-    const yearsStr = years > 1 ? "years" : "year";
-    const monthsStr = months > 1 ? "months" : "month";
+        to.toLowerCase() === "present"
+            ? DateTime.local().startOf("day")
+            : DateTime.fromISO(to);
+    const diff = toDate.diff(fromDate, ["years", "months"]).toObject();
+    const years = Math.floor(diff.years || 0);
+    const months = Math.floor(diff.months || 0);
 
     const result: string[] = [];
-    if (years > 0) result.push(`${years} ${yearsStr}`);
-    if (months > 0) result.push(`${months} ${monthsStr}`);
-    return result.join(", ");
-};
 
-const getToday = () => {
-    const todayObj = DateTime.local();
-    const {day, month, year} = todayObj;
-    const dayString = day < 10 ? "0" + day : day.toString();
-    const monthString = month < 10 ? "0" + month : month.toString();
-    const yearString = year.toString();
-    return DateTime.fromISO(`${yearString}-${monthString}-${dayString}`);
+    if (years > 0) {
+        result.push(`${years} ${pluralize("year", years)}`);
+    }
+
+    if (months > 0) {
+        result.push(`${months} ${pluralize("month", months)}`);
+    }
+
+    return result.join(", ");
 };
